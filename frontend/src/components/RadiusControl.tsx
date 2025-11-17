2a56579212fceb85028eef88bcca4e5e96d56905
[snapdev] <git@snapdev.sh>
2025-11-16 21:43:07 -0800

    [snapdev] Updated slider labels to show lower and upper bounds - wrote 1 file(s)

--- frontend/src/components/RadiusControl.tsx
+++ frontend/src/components/RadiusControl.tsx
@@ -42,11 +42,8 @@ export function RadiusControl({
         />
         
         <div className="flex justify-between text-xs text-gray-500">
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span>5</span>
-          <span>8</span>
+          <span>1 block</span>
+          <span>8 blocks</span>
         </div>
       </div>
     </div>
